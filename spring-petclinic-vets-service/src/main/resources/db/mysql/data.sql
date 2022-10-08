f092dba795bc63b2c005c457c5aa7306db6646fc
avcimehmet <trainitforfuture@gmail.com>
2022-10-08 10:46:02 +0000

    bahattin & Kemal hocas are added

--- spring-petclinic-vets-service/src/main/resources/db/mysql/data.sql
+++ spring-petclinic-vets-service/src/main/resources/db/mysql/data.sql
@@ -1,9 +1,11 @@
-INSERT IGNORE INTO vets VALUES (1, 'James', 'Carter');
-INSERT IGNORE INTO vets VALUES (2, 'Helen', 'Leary');
-INSERT IGNORE INTO vets VALUES (3, 'Linda', 'Douglas');
-INSERT IGNORE INTO vets VALUES (4, 'Rafael', 'Ortega');
-INSERT IGNORE INTO vets VALUES (5, 'Henry', 'Stevens');
-INSERT IGNORE INTO vets VALUES (6, 'Sharon', 'Jenkins');
+INSERT IGNORE INTO vets VALUES (1, 'BAHATTIN', 'ADRIAN');
+INSERT IGNORE INTO vets VALUES (2, 'KEMAL', 'HOCAM');
+INSERT IGNORE INTO vets VALUES (3, 'James', 'Carter');
+INSERT IGNORE INTO vets VALUES (4, 'Helen', 'Leary');
+INSERT IGNORE INTO vets VALUES (5, 'Linda', 'Douglas');
+INSERT IGNORE INTO vets VALUES (6, 'Rafael', 'Ortega');
+INSERT IGNORE INTO vets VALUES (7, 'Henry', 'Stevens');
+INSERT IGNORE INTO vets VALUES (8, 'Sharon', 'Jenkins');
 
 INSERT IGNORE INTO specialties VALUES (1, 'radiology');
 INSERT IGNORE INTO specialties VALUES (2, 'surgery');
